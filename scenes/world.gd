1e4787574db38caddd7eb3def63589ab415a0fc3
Pablo <pablohardouin02@gmail.com>
2024-06-04 23:12:43 +0200

    add multiple input system

---
+++ scenes/world.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+@export var player_scene: PackedScene
+
+var keyboards = []
+var controllers = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event is InputEventKey:
+		if event.device not in keyboards:
+			keyboards.append(event.device)
+			var player = player_scene.instantiate()
+			player.control_device = event.device
+			player.control_type = 0
+			player.position = $SpawnLocation.position
+			add_child(player)
+	if event is InputEventJoypadButton or event is InputEventJoypadMotion:
+		if event.device not in controllers:
+			controllers.append(event.device)
+			var player = player_scene.instantiate()
+			player.control_device = event.device
+			player.control_type = 1
+			player.position = $SpawnLocation.position
+			add_child(player)
