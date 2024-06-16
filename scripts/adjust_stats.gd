8888825ed496d822321aa6efbb99402961234d2f
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-06-16 23:06:19 +0200

    GD tool UI complete

--- scripts/adjust_stats.gd
+++ scripts/adjust_stats.gd
@@ -1,11 +1,27 @@
 extends CanvasLayer
 
 var is_active := false
+var last_open_file_path : String = "user://ProjetPixelGDdata.txt"
 @onready var char_selector := $Adjuster/VBoxContainer/OptionButton
+@onready var saved_path_node := $Adjuster/VBoxContainer/HBoxContainer/LabelLastPath
+var variable_adjusters : Array = []
+var variables_data : Dictionary = {}
 
 func _ready():
+	for c in $Adjuster/VBoxContainer.get_children():
+		if c.has_method("_on_description_changed"):
+			variable_adjusters.append(c)
 	for ev in PlayerCharacter.Evolutions.keys():
 		char_selector.add_item(ev)
+		var evolution_data = {}
+		var curr_ev = PlayerCharacter.Evolutions[ev]
+		var p : PlayerCharacter = PlayerCharacter.EvolutionCharacters[curr_ev].instantiate()
+		for adj : VariableAdjuster in variable_adjusters:
+			evolution_data[adj.variable_name] = p.get(adj.variable_name)
+			if ev == "CEO":
+				adj.variable_default_value = evolution_data[adj.variable_name]
+		p.queue_free()
+		variables_data[ev] = evolution_data
 	char_selector.select(0)
 	set_adjuster_active(false)
 
@@ -15,3 +31,11 @@ func _on_button_pressed():
 func set_adjuster_active(new_active : bool):
 	is_active = new_active
 	$Adjuster.visible = is_active
+
+func _on_button_save_pressed():
+	var save_game = FileAccess.open(last_open_file_path, FileAccess.WRITE)
+	save_game.store_line(JSON.stringify(variables_data))
+	saved_path_node.text = "saved to: " + last_open_file_path
+
+func _on_option_button_item_selected(index):
+	$Adjuster/VBoxContainer/OptionButton.get_item_text(index)
---
+++ scripts/variable_adjuster.gd
@@ -0,0 +1,39 @@
+@tool
+extends HBoxContainer
+class_name VariableAdjuster
+
+@export var variable_name := "HaelthPionts" : set=_on_var_name_changed
+@export var variable_description := "descritpion" : set=_on_description_changed
+@export var is_integer := false
+
+var value = -1
+var target_object : PlayerCharacter.Evolutions = PlayerCharacter.Evolutions.CEO
+var variable_default_value = -1 : set=_on_default_value_changed
+
+func _ready():
+	if is_integer:
+		$SpinBox.rounded = true
+		$LabelName.text = variable_name
+		$SpinBox.step = 1
+
+func _on_default_value_changed(new_val):
+	$SpinBox.value = new_val
+	variable_default_value = new_val
+
+func _on_description_changed(new_val : String):
+	variable_description = new_val
+	$Description.text = variable_description
+
+func _on_var_name_changed(new_val : String):
+	variable_name = new_val
+	$LabelName.text = variable_name
+
+func _on_button_reset_pressed():
+	$SpinBox.value = variable_default_value
+
+func _on_spin_box_value_changed(new_val : float):
+	if is_integer:
+		value = int(new_val)
+	else:
+		value = new_val
+
