b49abd8095c1262ef7d82df50d5887a7dec168a2
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-06-26 23:37:32 +0200

    fix freezeframe only processes first freezeframe

--- scenes/Utilities/FreezeFrame.gd
+++ scenes/Utilities/FreezeFrame.gd
@@ -1,6 +1,7 @@
 extends Node
 class_name FreezeFrame
 
+var added_freeze_frame := 0.0
 var in_freeze_frame := false
 
 func _ready():
@@ -8,6 +9,7 @@ func _ready():
 
 func freeze(duration : float = 0.15):
 	if in_freeze_frame:
+		added_freeze_frame += duration
 		return
 	in_freeze_frame = true
 	var previous_time_scale := Engine.time_scale
@@ -15,5 +17,9 @@ func freeze(duration : float = 0.15):
 	await get_tree().create_timer(duration, true, false, true).timeout
 	Engine.time_scale = previous_time_scale
 	in_freeze_frame = false
+	if added_freeze_frame > 0.0:
+		var new_freeze_frame := added_freeze_frame
+		added_freeze_frame = 0.0
+		freeze(new_freeze_frame)
 
 
