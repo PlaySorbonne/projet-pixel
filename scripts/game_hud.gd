34b2cc8cb3b4e6a0864df5f38842258f31c8b870
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-06-26 16:52:27 +0200

    added very basic hud

--- scripts/game_hud.gd
+++ scripts/game_hud.gd
@@ -1,11 +1,11 @@
 extends CanvasLayer
+class_name GameHud
 
 const PLAYER_PORTRAIT = preload("res://scenes/Menus/GameUI/player_portrait.tscn")
 
-func _ready():
-	pass
 
 func add_players():
-	for p in GameInfos.players:
+	for p : PlayerCharacter in GameInfos.players:
 		var portrait : PlayerPortrait = PLAYER_PORTRAIT.instantiate()
 		$HBox.add_child(portrait)
+		portrait.initialize_portrait(p.player_ID)
--- scripts/player_portrait.gd
+++ scripts/player_portrait.gd
@@ -2,10 +2,8 @@ extends Control
 class_name PlayerPortrait
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+func initialize_portrait(player_num : int):
+	$TextureBackground.modulate = PlayerCharacter.PLAYER_COLORS[player_num]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
