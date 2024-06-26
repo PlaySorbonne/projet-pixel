5c3c1f19f605ef728c3c5d47501d2960be4201b6
Pablo <contact@pablo.town>
2024-06-26 22:42:31 +0200

    fix thing

--- scripts/GameInfos.gd
+++ scripts/GameInfos.gd
@@ -16,9 +16,6 @@ func reset_game_infos():
 	players = []
 	player_colors = []
 	number_of_players = 0
-	
-func add_player(default_player: PackedScene):
-	
 
 # add stats (damage, kills, deaths, time in the air, time on the ground, distance, 
 # accuracy, evolution...)
