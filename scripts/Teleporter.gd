810c89ab467adb1c602862cb32fa41ed8dcedfef
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-07-17 19:30:12 +0200

    renamed téléporteur to Elevator and added Teleporters

---
+++ scripts/Teleporter.gd
@@ -0,0 +1,16 @@
+extends Area2D
+class_name Teleporter
+
+@export var otherTeleporter : Teleporter
+var ignored_objects : Array = []
+
+func _on_body_entered(body : Node2D):
+	var body_index = ignored_objects.find(body)
+	if body_index > 0:
+		ignored_objects.remove_at(body_index)
+		return
+	otherTeleporter.ignored_objects.append(body)
+	var vector_to_body : Vector2 = body.global_position - global_position
+	body.global_position = otherTeleporter.global_position + vector_to_body
+	await get_tree().create_timer(0.25).timeout
+	otherTeleporter.ignored_objects.erase(body)
