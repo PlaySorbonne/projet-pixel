63ecb6ca517771f75b740a55e82f70accded9208
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-08-27 15:05:51 +0200

    improved main menu

--- scripts/TitleScreen.gd
+++ scripts/TitleScreen.gd
@@ -2,6 +2,58 @@ extends Control
 
 signal ButtonStartPressed
 
+const position_x_normal := 250.0
+const position_x_selected := 300.0
+const button_colors := [Color.DARK_RED, Color.GOLD, Color.WEB_GRAY, Color.ROYAL_BLUE]
+
+@onready var buttons := [
+	$StartButton,
+	$Vault,
+	$Settings,
+	$Credits
+]
+var selected_button := 0
+
 func _on_start_button_pressed():
 	emit_signal("ButtonStartPressed")
 	$StartButton.disabled = true
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("down") or Input.is_action_just_pressed("right"):
+		var new_button := selected_button + 1
+		if new_button >= len(buttons):
+			new_button = 0
+		select_button(new_button)
+	elif Input.is_action_just_pressed("up") or Input.is_action_just_pressed("left"):
+		var new_button := selected_button - 1
+		if new_button < 0:
+			new_button = len(buttons) - 1
+		select_button(new_button)
+	elif Input.is_action_just_pressed("attack") or Input.is_action_just_pressed("special"):
+		confirm_button()
+
+func select_button(button_number : int):
+	var old_button : Button = buttons[selected_button]
+	var new_button : Button = buttons[button_number]
+	selected_button = button_number
+	
+	var t2 : Tween = get_tree().create_tween().set_ease(Tween.EASE_OUT)
+	t2.tween_property(new_button, "position", Vector2(position_x_selected, new_button.position.y), 0.15)
+	t2.set_parallel()
+	t2.tween_property(new_button, "scale", Vector2(1.15, 1.15), 0.15)
+	t2.tween_property(new_button, "modulate", button_colors[button_number], 0.15)
+
+	await get_tree().create_timer(0.05).timeout
+
+	var t1 : Tween = get_tree().create_tween().set_ease(Tween.EASE_OUT)
+	t1.tween_property(old_button, "position", Vector2(position_x_normal, old_button.position.y), 0.2)
+	t1.set_parallel()
+	t1.tween_property(old_button, "scale", Vector2.ONE, 0.2)
+	t1.tween_property(old_button, "modulate", Color.WHITE, 0.2)
+	
+	
+
+func confirm_button():
+	var b : Button = buttons[selected_button]
+	
