9711ccdbae5c28e983d7434ee88ce38ed31f6304
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-09-17 01:22:01 +0200

    sleep

--- scripts/character_pointer.gd
+++ scripts/character_pointer.gd
@@ -4,5 +4,17 @@ class_name CharacterPointer
 @onready var default_pos = position
 @onready var parent_character : Node2D = get_parent()
 
+func _ready():
+	pass
+	# TODO
+	# count total health and add how many progress bar you need, 
+	# put them on good positions and fill em up real good uwu
+
+# TODO
+# also add function to take damage
+# maybe something to shake the whole bar when taking damage
+# and blink when total is 1 HP (and not weeb ? although weeb 
+# constantly blinking might be fun
+
 func _process(_delta):
 	global_position = parent_character.global_position + default_pos
--- scripts/healthbar_unit.gd
+++ scripts/healthbar_unit.gd
@@ -6,19 +6,28 @@ const VAL_TO_TIP_POS_COEFF := 20.0
 
 @export var health : int = 5
 
+
+func set_health_value(new_val : int):
+	health = new_val
+	_update_healthbar()
+
 func damage(val : int):
-	health -= val
+	for _i in val:
+		health -= 1
+		_update_healthbar()
+		await get_tree().create_timer(0.1).timeout
+	if health > 0:
+		hit_effect()
+
+func _update_healthbar():
+	value = VAL_TO_PROGRESS[health]
 	if health == 0.0:
 		$TextureProgressTip.visible = false
 	else:
 		$TextureProgressTip.visible = true
 		$TextureProgressTip.position.x = DEFAULT_TIP_COLOR * health
-	value = VAL_TO_PROGRESS[health]
 
-func heal(val : int):
-	pass
-
-func hit_effect(number := 3, time := 0.25, hit_color := Color.WHITE):
+func hit_effect(number := 3, time := 0.2, hit_color := Color.WHITE):
 	for _i in range(number):
 		$TextureProgressTip.modulate = hit_color
 		await get_tree().create_timer(time).timeout
