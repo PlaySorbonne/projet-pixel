487efdaf977e63e35b408c0a4bc87e313cee0009
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-09-17 11:59:53 +0200

    better hit animation on healthbar

--- scripts/healthbar_unit.gd
+++ scripts/healthbar_unit.gd
@@ -2,7 +2,8 @@ extends TextureProgressBar
 
 const VAL_TO_PROGRESS = [0.0, 0.0, 1.5, 2.3, 3.3, 4.5]
 const DEFAULT_TIP_COLOR = Color(0.812, 0.0, 0.0)
-const VAL_TO_TIP_POS_COEFF := 20.0
+const TIP_POS_COEFF := 20.0
+const PROGRESS_TIP_TEXTURE = preload("res://scenes/Menus/GameUI/texture_progress_tip.tscn")
 
 @export var health : int = 5
 
@@ -12,12 +13,16 @@ func set_health_value(new_val : int):
 	_update_healthbar()
 
 func damage(val : int):
-	for _i in val:
-		health -= 1
-		_update_healthbar()
-		await get_tree().create_timer(0.1).timeout
-	if health > 0:
-		hit_effect()
+	var new_tips : Array[TextureRect] = []
+	for h in range(health, health-val, -1):
+		var health_slot := PROGRESS_TIP_TEXTURE.instantiate()
+		health_slot.modulate = Color.WHITE
+		health_slot.position = Vector2(TIP_POS_COEFF*h, 0.0)
+		add_child(health_slot)
+		new_tips.append(health_slot)
+	health -= val
+	_update_healthbar()
+	hit_effect(new_tips)
 
 func _update_healthbar():
 	value = VAL_TO_PROGRESS[health]
@@ -27,9 +32,7 @@ func _update_healthbar():
 		$TextureProgressTip.visible = true
 		$TextureProgressTip.position.x = DEFAULT_TIP_COLOR * health
 
-func hit_effect(number := 3, time := 0.2, hit_color := Color.WHITE):
-	for _i in range(number):
-		$TextureProgressTip.modulate = hit_color
-		await get_tree().create_timer(time).timeout
-		$TextureProgressTip.modulate = DEFAULT_TIP_COLOR
-		await get_tree().create_timer(time).timeout
+func hit_effect(lost_slots : Array[TextureRect]):
+	for s : TextureRect in lost_slots:
+		await get_tree().create_timer(0.25).timeout
+		s.queue_free()
