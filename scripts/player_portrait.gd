7d22b0d3a5589c0271f72298236e1c93ed043fe4
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-09-19 01:43:27 +0200

    added ceo portrait

--- scripts/player_portrait.gd
+++ scripts/player_portrait.gd
@@ -2,7 +2,7 @@ extends Control
 class_name PlayerPortrait
 
 const PLAYER_PORTRAITS = [
-	preload("res://resources/images/champgrumpf.png"),
+	preload("res://resources/images/characters/CEO/CEO_portrait.png"),
 	preload("res://resources/images/characters/fox/fox_portrait.png"),
 	preload("res://resources/images/characters/Sheep/mouton_portrait.png"),
 	preload("res://resources/images/characters/chicken/chicken_portrait.png"),
