5232e95472147fc7318d33ab7a1451b43b64a900
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-09-18 23:04:38 +0200

    setting game values in game creation screen

--- scripts/game_creation_screen.gd
+++ scripts/game_creation_screen.gd
@@ -1,6 +1,9 @@
 extends Control
 class_name GameCreationScreen
 
+const MUSIC_NAMES = [
+	"Secret Knowledge"
+]
 const GAME_MODE_TITLES = [
 	"BRAWL"
 ]
@@ -27,9 +30,15 @@ var player_selectors : Array[PlayerSelection] = []
 
 func _ready():
 	GameInfos.reset_game_infos()
+	set_game_widgets()
 	transition.end_screen_transition()
 	if GameInfos.players_data.keys().size() != 0:
 		reload_old_game_infos()
+
+func set_game_widgets():
+	set_gamemode(GameInfos.selected_gamemode)
+	set_music(GameInfos.selected_music)
+	set_level(GameInfos.selected_level)
 
 func reload_old_game_infos():
 	for id: int in GameInfos.players_data.keys():
@@ -59,7 +68,6 @@ func create_player_infos(index : int):
 	player_infos.connect("player_removed", remove_player)
 
 func add_player(device_type : int, device : int):
-	var is_keyboard = (device_type == 0)
 	var player : PlayerCharacter = DEFAULT_PLAYER.instantiate()
 	var player_index := player.player_ID
 	player.control_device = device
@@ -94,13 +102,26 @@ func _on_button_test_music_pressed():
 	set_test_music(not music_tester.playing)
 
 func _on_music_selector_option_changed(new_option : int):
-	$ButtonTestMusic.text = $MusicSelector.get_text()
-	GameInfos.selected_music = new_option
+	set_music(new_option)
 
-func _on_game_mode_selector_option_changed():
-	pass # Replace with function body.
+func set_music(music : int):
+	$ButtonTestMusic.text = MUSIC_NAMES[music]
+	GameInfos.selected_music = music
 
-func remove_player(selector : PlayerSelection, index : int):
+func _on_game_mode_selector_option_changed(new_option : int):
+	set_gamemode(new_option)
+
+func set_gamemode(gamemode : int):
+	$LabelGameModeName.text = GAME_MODE_TITLES[gamemode]
+	$LabelGameModeDescription.text = GAME_MODE_DESCRIPTIONS[gamemode]
+
+func _on_level_selector_option_changed(new_option : int):
+	set_level(new_option)
+
+func set_level(level : int):
+	print("TODO : set level icon system in game_creation_screen")
+
+func remove_player(selector : PlayerSelection, _index : int):
 	var pos_in_array := player_selectors.find(selector)
 	var duration := 0.3
 	var tween := create_tween().set_parallel()
@@ -108,9 +129,6 @@ func remove_player(selector : PlayerSelection, index : int):
 		controllers.erase(selector.control_index)
 	else:
 		keyboards.erase(selector.control_index)
-	print("Remove Player")
-	print("\tcontrollers = " + str(controllers))
-	print("\tkeyboards = " + str(keyboards))
 	for i in range(pos_in_array+1, player_selectors.size()):
 		var s : PlayerSelection = player_selectors[i]
 		tween.tween_property(s, "position", s.position - PLAYER_INFOS_POS_OFFSET, duration)
