62790239e6ba3309ac788958878c8bea34d32458
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-09-22 14:53:08 +0200

    added (not working) click function to simulated cursor

--- scripts/player_menu_cursor.gd
+++ scripts/player_menu_cursor.gd
@@ -27,26 +27,45 @@ func _input(event : InputEvent):
 	elif control_type == 1:
 		is_correct_control_type = (event is InputEventJoypadButton) or (event is InputEventJoypadMotion)
 		
-	if is_correct_control_type && event.device == control_device:
-		if event.is_action_pressed("right"):
-			right_pressed = true
-		elif event.is_action_released("right"):
-			right_pressed = false
-		if event.is_action_pressed("left"):
-			left_pressed = true
-		elif event.is_action_released("left"):
-			left_pressed = false
-		if event.is_action_pressed("up"):
-			up_pressed = true
-		elif event.is_action_released("up"):
-			up_pressed = false
-		if event.is_action_pressed("down"):
-			down_pressed = true
-		elif event.is_action_released("down"):
-			down_pressed = false
-	
-		# TODO:
-		# add ways to click buttons
+	if not (is_correct_control_type && event.device == control_device):
+		return
+	if event.is_action_pressed("right"):
+		right_pressed = true
+	elif event.is_action_released("right"):
+		right_pressed = false
+	if event.is_action_pressed("left"):
+		left_pressed = true
+	elif event.is_action_released("left"):
+		left_pressed = false
+	if event.is_action_pressed("up"):
+		up_pressed = true
+	elif event.is_action_released("up"):
+		up_pressed = false
+	if event.is_action_pressed("down"):
+		down_pressed = true
+	elif event.is_action_released("down"):
+		down_pressed = false
+
+	if event.is_action_pressed("attack") or event.is_action_pressed(
+					"special") or event.is_action_pressed("jump"):
+		call_deferred("simulate_click")
+		$AudioStreamPlayer.play()
+		var tween := create_tween()
+		tween.tween_property(self, "scale", Vector2(1.5, 1.5), 0.1)
+		tween.tween_property(self, "scale", Vector2.ONE, 0.1)
+
+func simulate_click():
+	var mouse := InputEventMouseButton.new()
+	mouse.global_position = global_position
+	mouse.button_index = MOUSE_BUTTON_LEFT
+	mouse.button_mask = MOUSE_BUTTON_MASK_LEFT
+	mouse.pressed = true
+	Input.parse_input_event(mouse)
+	await get_tree().process_frame
+	mouse.pressed = false
+	Input.parse_input_event(mouse)
+	print("click : global_position = " + str(mouse.global_position))
+	print("click : mouse_position = " + str(get_global_mouse_position()))
 
 func _process(delta : float):
 	var velocity := Vector2.ZERO
