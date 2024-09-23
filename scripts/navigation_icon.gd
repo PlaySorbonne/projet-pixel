10393ee8d784a4dba8c68f8e14c881d15dd04b37
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-09-24 01:08:25 +0200

    added navigation icons

---
+++ scripts/navigation_icon.gd
@@ -0,0 +1,28 @@
+extends TextureRect
+
+@export var is_current_screen := false:
+	set(value):
+		if value == is_current_screen:
+			return
+		is_current_screen = value
+		var new_color : Color
+		if is_current_screen:
+			new_color = Color.GOLD
+		else:
+			new_color = Color.WHITE
+		var tween := create_tween()
+		tween.tween_property(self, "modulate", new_color, 0.25)
+
+
+func _ready():
+	$TextureRect.texture = texture
+
+func _tween_scale(new_scale : Vector2):
+	var tween := create_tween()
+	tween.tween_property(self, "scale", new_scale, 0.2)
+
+func _on_focus_entered():
+	_tween_scale(Vector2(1.25, 1.25))
+
+func _on_focus_exited():
+	_tween_scale(Vector2.ONE)
