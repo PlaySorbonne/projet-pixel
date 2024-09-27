d904ac11bc1a2e3dedf9231564db86ce74d27a8c
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-09-27 16:59:36 +0200

    created vault data singleton

--- scripts/vault_data.gd
+++ scripts/vault_data.gd
@@ -2,42 +2,106 @@ extends Node
 
 enum ItemTypes {MultGameMode, Map, Minigame, Artwork, Music}
 
-var VAULT_ITEMS : Array[VaultItem] = [
+var VAULT_GAMEMODES : Array[VaultItem] = [
+	VaultItem.new(
+		"Brawl",
+		"BRAWL_DESC",
+		"",
+		null
+	),
+	VaultItem.new(
+		"Shonen Battle",
+		"SHONEN_BATTLE_DESC",
+		"",
+		null
+	),
+	VaultItem.new(
+		"Corporate Chaos",
+		"CORPORATE_CHAOS_DESC",
+		"",
+		null
+	),
 	VaultItem.new(
 		"Otaku Outbreak", 
-		"desc", 
-		ItemTypes.MultGameMode, 
+		"OTAKU_OUTBREAK_DESC", 
 		"", 
 		load("res://resources/images/objects/anime/anime_one_champsu.png")
 	),
 	VaultItem.new(
 		"Otaku Overdrive",
-		"desc",
-		ItemTypes.MultGameMode,
-		"",
-		null
-	),
-	VaultItem.new(
-		"",
-		"",
-		ItemTypes.Map,
+		"OTAKU_OVERDRIVE_DESC",
 		"",
 		null
 	)
 ]
+
+var VAULT_MINIGAMES : Array[VaultItem] = [
+	VaultItem.new(
+		"Wild West Shootout",
+		"WILD_WEST_DESC",
+		"",
+		null
+	),
+	VaultItem.new(
+		"Weebocalypse",
+		"WEEBOCALYPSE_DESC",
+		"",
+		null
+	),
+	VaultItem.new(
+		"Retail Relaxation",
+		"RETAIL_RELAXATION_DESC",
+		"",
+		null
+	),
+	VaultItem.new(
+		"Rooster's",
+		"ROOSTER_S_DESC",
+		"",
+		null
+	)
+]
+
+var VAULT_LEVELS : Array[VaultItem] = [
+	VaultItem.new(
+		"Wild West Shootout",
+		"WILD_WEST_DESC",
+		"",
+		null
+	)
+]
+
+var VAULT_ARTWORK : Array[VaultItem] = [
+	VaultItem.new(
+		"",
+		"",
+		"",
+		null
+	)
+]
+
+var VAULT_MUSIC : Array[VaultItem] = [
+	VaultItem.new(
+		"",
+		"",
+		"",
+		null
+	)
+]
+
+
+
 var unlocked_items : Array[String] = []
 
 class VaultItem:
 	var item_name := ""
 	var item_description := ""
-	var item_type : ItemTypes
 	var item_texture : Texture
 	var item_path : String
 	
 	func _init(_item_name : String, _item_description : String,
-	 _item_type : ItemTypes, _item_path : String, _texture : Texture):
+	 _item_path : String, _texture : Texture):
 		item_name = _item_name
 		item_description = _item_description
-		item_type = _item_type
 		item_path = _item_path
 		item_texture = _texture
