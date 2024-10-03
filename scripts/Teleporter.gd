4a3e454250ac964161dde22d44254478eed83b7b
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-10-03 21:24:47 +0200

    fixed teleporter + training levels

--- scripts/Teleporter.gd
+++ scripts/Teleporter.gd
@@ -5,10 +5,19 @@ class_name Teleporter
 @export var teleport_offset := Vector2(0.0, -500.0):
 	set(value):
 		teleport_offset = value
-		$Marker2D.position = value
+		$Marker2D.global_position = position + value
+@export var keep_offset := true
+
+func _ready():
+	$Marker2D.global_position = position + teleport_offset
 
 func _on_body_entered(body : Node2D):
-	if body.has_method("force_position"):
-		body.force_position(body.position + teleport_offset.rotated(rotation))
+	var new_pos : Vector2
+	if keep_offset:
+		new_pos = body.position + teleport_offset.rotated(rotation)
 	else:
-		body.position += teleport_offset.rotated(rotation)
+		new_pos = position + teleport_offset.rotated(rotation)
+	if body.has_method("force_position"):
+		body.force_position(new_pos)
+	else:
+		body.position = new_pos
