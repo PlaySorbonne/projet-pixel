a9c2ce302e82eb54367c13c6376dce1de1336a0d
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-10-03 20:38:34 +0200

    fixed teleporters + reworked high ground

--- scripts/Teleporter.gd
+++ scripts/Teleporter.gd
@@ -1,16 +1,11 @@
+@tool
 extends Area2D
 class_name Teleporter
 
-@export var otherTeleporter : Teleporter
-var ignored_objects : Array = []
+@export var teleport_offset := Vector2(0.0, -500.0):
+	set(value):
+		teleport_offset = value
+		$Marker2D.position = value
 
 func _on_body_entered(body : Node2D):
-	var body_index = ignored_objects.find(body)
-	if body_index >= 0:
-		ignored_objects.remove_at(body_index)
-		return
-	otherTeleporter.ignored_objects.append(body)
-	var vector_to_body : Vector2 = body.global_position - global_position
-	body.global_position = otherTeleporter.global_position + vector_to_body
-	await get_tree().create_timer(0.15).timeout
-	otherTeleporter.ignored_objects.erase(body)
+	body.position += teleport_offset.rotated(rotation)
