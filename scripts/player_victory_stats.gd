63747db7a3ad478ae7da39c6057552839620bfe1
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-26 22:33:12 +0100

    wip victory screen

--- scripts/player_victory_stats.gd
+++ scripts/player_victory_stats.gd
@@ -1,6 +1,18 @@
 extends VBoxContainer
 class_name PlayerVictoryStats
 
+"""
+Specific titles:
+	Hokage: most damage inflicted
+	Pirate King: 
+	Luigi: did nothing
+	Super Sayen: longest time as ascended weeb
+
+Random titles:
+	G
+
+
+"""
 
 func set_player_evolution(current_ev : int):
 	str(PlayerCharacter.Evolutions.keys()[current_ev])
