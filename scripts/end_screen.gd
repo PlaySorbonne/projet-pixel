9fcd6353d4865dc1ee8147b91a2a864445f13450
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-26 17:52:37 +0100

    bugfix

--- scripts/end_screen.gd
+++ scripts/end_screen.gd
@@ -1,15 +1,17 @@
 extends Control
 class_name EndScreen
 
+signal end_game_finished
+
 const LABEL_END_SCREEN_RES := preload("res://scenes/Menus/GameUI/label_player_end_screen.tscn")
 
 var is_end_game := false
-var current_end_step := 0
+var current_end_step := 1
+var end_finished := false
 
 @onready var player_stats_node : GridContainer = $PlayerStats
 
 func _ready() -> void:
-	set_process(false)
 	print("WORLD -> CANVASLAYER -> END_SCREEN: REMEMBER TO HIDE END_SCREEN")
 	print("AND SHOW SCREEN_TRANSITION")
 	print("AND SET END_SCREEN PROCESS TO WHEN_PAUSED")
@@ -18,19 +20,33 @@ func init_end_screen(winner_id : int, players_stats : Dictionary) -> void:
 	is_end_game = true
 	set_process(true)
 	var arr_stats : Array[PlayerStats] = [players_stats[winner_id]]
-	for p_stats : PlayerStats in players_stats:
+	for p_stats : PlayerStats in players_stats.values():
 		p_stats.set_death_based_on_winner(winner_id)
 		if p_stats.player_id != winner_id:
 			arr_stats.append(p_stats)
 	for p_stats : PlayerStats in arr_stats:
 		for s : String in p_stats.get_stats_as_array():
 			var l : Label = LABEL_END_SCREEN_RES.instantiate()
-			l.text = p_stats.s
+			l.text = s
 			player_stats_node.add_child(l)
-		
+	$AnimationEndSteps.play("end_enter")
+	visible = true
 
 func _process(delta: float) -> void:
+	if not is_end_game:
+		return
 	if Input.is_action_just_pressed("jump") or Input.is_action_just_pressed("special"):
 		current_end_step += 1
+		execute_current_step()
 	elif Input.is_action_just_pressed("attack"):
-		current_end_step -= 1
+		current_end_step = max(0, current_end_step-1)
+		execute_current_step()
+
+func execute_current_step():
+	match current_end_step:
+		0:
+			$AnimationEndSteps.play_backwards("end_enter")
+		1:
+			$AnimationEndSteps.play("end_enter", -1, 1.0, false)
+		2:
+			emit_signal("end_game_finished")
