8d585d8576125edf6811df2fad47e2facf030c32
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-26 15:25:01 +0100

    added end screen animations

--- scripts/end_screen.gd
+++ scripts/end_screen.gd
@@ -2,9 +2,20 @@ extends Control
 class_name EndScreen
 
 var is_end_game := false
+var current_end_step := 0
 
 func _ready() -> void:
-	print("WORLD -> CANVASLAYER -> END_SCREEN: REMEMBER TO HIDE END_SCREEN AND SHOW SCREEN_TRANSITION")
+	set_process(false)
+	print("WORLD -> CANVASLAYER -> END_SCREEN: REMEMBER TO HIDE END_SCREEN")
+	print("AND SHOW SCREEN_TRANSITION")
+	print("AND SET END_SCREEN PROCESS TO WHEN_PAUSED")
 
 func init_end_screen() -> void:
 	is_end_game = true
+	set_process(true)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("jump") or Input.is_action_just_pressed("special"):
+		current_end_step += 1
+	elif Input.is_action_just_pressed("attack"):
+		current_end_step -= 1
