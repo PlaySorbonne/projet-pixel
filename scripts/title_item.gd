b21f62e84468f05fdc7617b78e2b7f2e670e574d
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-28 00:24:16 +0100

    wip title effects

--- scripts/title_item.gd
+++ scripts/title_item.gd
@@ -6,17 +6,37 @@ enum Rarities {Common, Rare, Legendary}
 var item_rarity := Rarities.Common
 
 func set_title(title : String, rarity : Rarities) -> void:
-	var effect_intro : String = ""
-	var effect_outro : String = ""
+	const RARE_COLORS := ["purple", "red", "cadet_blue", "violet", "teal", "slate_blue",
+			"peru", "blue", "green", "dark_green"]
+	const COMMON_COLORS := ["gray", "dark_gray", "slate_gray", "light_gray", "light_blue", 
+			"salmon", "cyan", "aquamarine", "blanched_almond", "burlywood"]
+	var effects_intro : String = "[center]"
 	item_rarity = rarity
+	# add text effects depending on rarity and chance
 	match item_rarity:
 		Rarities.Common:
-			pass
+			if randi()%3 == 0:
+				if randi()%2 == 0:
+					var pulse_color : String = COMMON_COLORS.pick_random() + " ease=-2.0]"
+					effects_intro = effects_intro + "[pulse freq=1.0 color="+pulse_color
+				else:
+					var text_color : String = COMMON_COLORS.pick_random() + "]"
+					effects_intro = effects_intro + "[color=" + text_color
 		Rarities.Rare:
-			pass
+			if randi()%2 == 0:
+				var pulse_color : String = RARE_COLORS.pick_random() + "ff ease=-2.0]"
+				effects_intro = effects_intro + "[pulse freq=1.3 color="+pulse_color
+			else:
+				var text_color : String = RARE_COLORS.pick_random() + "ff]"
+				effects_intro = effects_intro + "[color=" + text_color
+			if randi()%2 == 0:
+				effects_intro = effects_intro + "[shake rate=20.0 level=5 connected=1]"
+			else:
+				effects_intro = effects_intro + "[tornado radius=2.5 freq=5.0 connected=1]"
 		Rarities.Legendary:
-			pass
-	$Label.text = effect_intro + title + effect_outro
+			effects_intro = effects_intro + "[tornado radius=3.5 freq=8.0 connected=1]"
+			effects_intro = effects_intro + "[rainbow freq=0.8 sat=0.5 val=0.9]"
+	$Label.text = effects_intro + title
 
 func anim_intro() -> void:
 	match item_rarity:
