49fd6666b058c60eeef1b8dcf93f99328d6e73e4
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-27 13:50:25 +0100

    added title arrays

--- scripts/player_victory_stats.gd
+++ scripts/player_victory_stats.gd
@@ -1,17 +1,45 @@
 extends VBoxContainer
 class_name PlayerVictoryStats
 
+
+
 """
 Specific titles:
-	Hokage: most damage inflicted
-	Pirate King: 
-	Luigi: did nothing
-	Super Sayen: longest time as ascended weeb
+	victory:
+		The No.1 Hero
+		Avatar
+		I'll be back
+		Super Sayen
+	
+	0 dmg given, 0 kills:
+		Luigi
+		Zero
+	
+	most damage inflicted:
+		Hokage
+	
+	never died:
+		FullMeta	l
+	
+	most kills:
+		Hero Killer
+		Shinigami
+
+Unique titles:
+	over 9000 dmg:
+		Over 9000
+	no dmg received, no death, over 10 kills & 100 dmg & victory:
+		The Strongest Man in the World
 
 Random titles:
-	G
-
-
+	Professor Layton
+	Pac-Man
+	Hero of Time
+	Procrastinator
+	Darth Weeb
+	Dora la Exploradora
+	Pirate King
+	King of Kongs
 """
 
 func set_player_evolution(current_ev : int):
