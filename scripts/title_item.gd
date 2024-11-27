6f7b843a1ed981cec317a4c6c04a5feefb3b957a
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-27 23:40:13 +0100

    wip functions to set effects, txt, anim depending on title rarity

--- scripts/title_item.gd
+++ scripts/title_item.gd
@@ -1,2 +1,28 @@
 extends Control
 class_name TitleItem
+
+enum Rarities {Common, Rare, Legendary}
+
+var item_rarity := Rarities.Common
+
+func set_title(title : String, rarity : Rarities) -> void:
+	var effect_intro : String = ""
+	var effect_outro : String = ""
+	item_rarity = rarity
+	match item_rarity:
+		Rarities.Common:
+			pass
+		Rarities.Rare:
+			pass
+		Rarities.Legendary:
+			pass
+	$Label.text = effect_intro + title + effect_outro
+
+func anim_intro() -> void:
+	match item_rarity:
+		Rarities.Common:
+			pass
+		Rarities.Rare:
+			pass
+		Rarities.Legendary:
+			pass
