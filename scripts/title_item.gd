490780c01d445f693323d2677a6b102a7b4d6a15
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-28 15:48:10 +0100

    adjust titles colors

--- scripts/title_item.gd
+++ scripts/title_item.gd
@@ -11,8 +11,8 @@ func _ready() -> void:
 	$Label.modulate = Color.TRANSPARENT
 
 func set_title(title : String, rarity : Rarities) -> void:
-	const RARE_COLORS := ["purple", "red", "cadet_blue", "violet", "teal", "slate_blue",
-			"peru", "blue", "green", "dark_green"]
+	const RARE_COLORS := ["red", "cadet_blue", "violet", "teal", "hot_pink", "khaki",
+			"peru", "sky_blue", "green", "lime_green", "orange"]
 	const COMMON_COLORS := ["gray", "dark_gray", "slate_gray", "light_gray", "light_blue", 
 			"salmon", "cyan", "aquamarine", "blanched_almond", "burlywood", "white", "white"]
 	var effects_intro : String = "[center]"
@@ -20,23 +20,16 @@ func set_title(title : String, rarity : Rarities) -> void:
 	# add text effects depending on rarity and chance
 	match item_rarity:
 		Rarities.Common:
-			#if randi()%2 == 0:
-				#var pulse_color : String = COMMON_COLORS.pick_random() + " ease=-2.0]"
-				#effects_intro = effects_intro + "[pulse freq=1.0 color="+pulse_color
-			#else:
 			var text_color : String = COMMON_COLORS.pick_random() + "]"
 			effects_intro = effects_intro + "[color=" + text_color
 		Rarities.Rare:
-			if randi()%2 == 0 and false:
+			if randi() % 2 == 0:
 				var pulse_color : String = RARE_COLORS.pick_random() + " ease=-2.0]"
 				effects_intro = effects_intro + "[pulse freq=1.3 color="+pulse_color
 			else:
 				var text_color : String = RARE_COLORS.pick_random()
 				effects_intro = effects_intro + "[color=" + text_color + "]"
-			if randi()%2 == 0 or true:
-				effects_intro = effects_intro + "[shake rate=20.0 level=5 connected=1]"
-			#else:
-				#effects_intro = effects_intro + "[tornado radius=2.5 freq=5.0 connected=1]"
+			effects_intro = effects_intro + "[shake rate=20.0 level=5 connected=1]"
 		Rarities.Legendary:
 			effects_intro = effects_intro + "[tornado radius=3.5 freq=8.0 connected=1]"
 			effects_intro = effects_intro + "[rainbow freq=0.8 sat=0.5 val=0.9]"
