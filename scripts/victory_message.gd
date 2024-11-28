0ee665170f5f9d19cf6ecc31ca8e80569fb57f7b
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-28 15:34:36 +0100

    fixed whoopsy daisy bug infini

--- scripts/victory_message.gd
+++ scripts/victory_message.gd
@@ -1,8 +1,10 @@
-@tool
 extends CanvasLayer
 class_name EndGameScreen
 
 func _ready():
 	$AnimationPlayer.play("end_anim")
+
+func remove():
+	$AnimationPlayer.play("remove")
 	await $AnimationPlayer.animation_finished
 	queue_free()
