be8dd897510affa88246f01bf43c836f9aac33d5
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-12-01 00:12:09 +0100

    wip gamemodes 2

---
+++ scripts/gamemodes/default_gamemode.gd
@@ -0,0 +1,13 @@
+extends BattleGameMode
+class_name DefaultGameMode
+
+
+func _init() -> void:
+	intial_evolution = PlayerCharacter.Evolutions.CEO
+	use_unique_stats_file = false
+
+func select_gamemode() -> void:
+	return
+
+func start_game() -> void:
+	pass
---
+++ scripts/gamemodes/gamemode_base.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name BattleGameMode
+
+var intial_evolution : PlayerCharacter.Evolutions = PlayerCharacter.Evolutions.CEO
+var use_unique_stats_file := false
+
+func select_gamemode() -> void:
+	return
+
+func start_game() -> void:
+	pass
