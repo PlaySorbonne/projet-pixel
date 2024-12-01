d87f22f1253a1a4fc3a85926490bbb3490b1e79e
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-12-01 23:55:53 +0100

    paused gamemode work

--- scripts/gamemodes/gamemode_base.gd
+++ scripts/gamemodes/gamemode_base.gd
@@ -4,6 +4,8 @@ class_name BattleGameMode
 @export var intial_evolution : PlayerCharacter.Evolutions = PlayerCharacter.Evolutions.CEO
 @export var use_unique_stats_file := false
 @export var unique_stats_file : String = ""
+@export var is_timed := false
+@export var time_limit := 300.0
 
 func select_gamemode() -> void:
 	return
