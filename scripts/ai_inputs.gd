ae81fd58476e909367782d2be352dc342544b63e
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-12-02 00:28:04 +0100

    wip enemy ai

---
+++ scripts/ai_inputs.gd
@@ -0,0 +1,33 @@
+extends Node
+class_name AI_Inputs
+
+
+var reaction_time := 0.1
+
+@onready var player : PlayerCharacter = self.get_parent()
+
+func _process(_delta: float) -> void:
+	pass
+	# actions:
+		# move (left/right/drop down)
+		# jump/stop_jump
+		# use special
+		# attack
+	# considerations:
+		# ascended weeb
+		# other players
+		# cassette
+		# platforms
+		# semi-solid platforms
+	
+	# COMMANDS:
+		# player.right_pressed
+		# player.left_pressed
+		# player.up_pressed
+		# player.down_pressed
+		
+		# player.jump()
+		# player.stop_jump()
+		
+		# player.attack()
+		# player.special()
---
+++ scripts/ai_inputs_ceo.gd
@@ -0,0 +1,28 @@
+extends AI_Inputs
+class_name AI_InputsCEO
+
+func _process(_delta: float) -> void:
+	pass
+	# actions:
+		# move (left/right/drop down)
+		# jump/stop_jump
+		# use special
+		# attack
+	# considerations:
+		# ascended weeb
+		# other players
+		# cassette
+		# platforms
+		# semi-solid platforms
+	
+	# COMMANDS:
+		# player.right_pressed
+		# player.left_pressed
+		# player.up_pressed
+		# player.down_pressed
+		
+		# player.jump()
+		# player.stop_jump()
+		
+		# player.attack()
+		# player.special()
