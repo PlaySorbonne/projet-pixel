60b9f2c2aacf9b574b7fa176ecc1230cfd39b204
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-12-02 12:29:09 +0100

    wip ai ceo behavior

--- scripts/ai_inputs.gd
+++ scripts/ai_inputs.gd
@@ -1,37 +1,56 @@
 extends Node
 class_name AI_Inputs
 
-const UPDATE_TIME_LIMIT := 1.0
+enum Behaviors {PutDistance, AttackNearest, AttackLowestHealth}
 
+const UPDATE_TIME_LIMIT := 0.7
+const MIN_TIME_BETWEEN_BEHAVIORS := 3.0
+
+var time_since_behavior_change := 0.0
 var update_time := 0.0
 var reaction_time := 0.1
 var enemy_ids : Array[int] = []
 var enemies : Array[PlayerCharacter] = []
 var enemy_distances : Array[float] = []
 var enemy_directions : Array[Vector2] = []
+var enemy_hitpoints : Array[int] = []
+var enemy_position_differences : Array[Vector2] = []
+#var enemy_velocities : Array[Vector2] = []
+var current_behavior : Behaviors = Behaviors.AttackNearest
 
 @onready var player : PlayerCharacter = self.get_parent()
-
 
 func _ready() -> void:
 	for p_id : int in GameInfos.players.keys():
 		if p_id != player.player_ID:
 			enemy_ids.append(p_id)
 
+func set_behavior() -> void:
+	time_since_behavior_change = 0.0
+	current_behavior = Behaviors.values().pick_random()
+
 func update_enemies(delta : float, force_update := false) -> void:
 	update_time -= delta
+	time_since_behavior_change += delta
+	if time_since_behavior_change >= MIN_TIME_BETWEEN_BEHAVIORS:
+		set_behavior()
 	if update_time > 0.0 and not force_update:
 		return
 	update_time = UPDATE_TIME_LIMIT
 	enemies = []
 	enemy_distances = []
 	enemy_directions = []
+	enemy_hitpoints = []
+	enemy_position_differences = []
 	var location := player.global_position
 	for id : int in enemy_ids:
-		var enemy_location : Vector2 = GameInfos.players[id].global_position
+		var current_enemy : PlayerCharacter = GameInfos.players[id]
+		var enemy_location : Vector2 = current_enemy.global_position
 		enemies.append(GameInfos.players[id])
 		enemy_distances.append(location.distance_squared_to(enemy_location))
 		enemy_directions.append(location.direction_to(enemy_location))
+		enemy_hitpoints.append(current_enemy.hitpoints)
+		enemy_position_differences.append(location - enemy_location)
 
 func _process(_delta: float) -> void:
 	pass
--- scripts/ai_inputs_ceo.gd
+++ scripts/ai_inputs_ceo.gd
@@ -1,10 +1,21 @@
 extends AI_Inputs
 class_name AI_InputsCEO
 
-func _process(_delta: float) -> void:
-	pass
+const CHARGE_MAX_Y_OFFSET := 20.0
+
+
+func _process(delta: float) -> void:
+	update_enemies(delta, false)
 	
-	
+	for i in range(enemies.size()):
+		var pos_diff : Vector2 = enemy_position_differences[i]
+		if abs(pos_diff.y) < CHARGE_MAX_Y_OFFSET:
+			if player.facing_right:
+				if 
+			player.special()
+
+
+
 	# actions:
 		# move (left/right/drop down)
 		# jump/stop_jump
