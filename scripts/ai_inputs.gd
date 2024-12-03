c28797f6adfeea275b35ec6b7166bb48aa051bc9
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-12-03 20:06:16 +0100

    fixed ai blocking on obstacles

--- scripts/ai_inputs.gd
+++ scripts/ai_inputs.gd
@@ -25,6 +25,7 @@ var time_since_attack := 0.0
 var time_since_jump := 0.0
 var update_time := 0.0
 var reacting_time := 0.0
+var time_since_blocked := 0.0
 
 var enemy_ids : Array[int] = []
 var enemies : Array[PlayerCharacter] = []
@@ -35,6 +36,8 @@ var enemy_position_differences : Array[Vector2] = []
 
 var chosen_enemy : int
 var special_enemy : Node2D = null
+var is_moving := false
+var last_position : Vector2
 
 @onready var player : PlayerCharacter = self.get_parent()
 
@@ -52,19 +55,19 @@ func set_difficulty(difficulty : Difficulty) -> void:
 			reaction_time = 0.8
 			min_time_between_specials = 6.0
 			min_time_between_attacks = 1.1
-			min_time_between_jumps = 2.8
+			min_time_between_jumps = 3.4
 			min_time_chosen_enemy = 5.0
 		Difficulty.Mid:
-			reaction_time = 0.3
+			reaction_time = 0.25
 			min_time_between_specials = 3.5
 			min_time_between_attacks = 0.15
-			min_time_between_jumps = 1.4
+			min_time_between_jumps = 1.7
 			min_time_chosen_enemy = 4.0
 		Difficulty.Hard:
 			reaction_time = 0.0
 			min_time_between_specials = 2.3
 			min_time_between_attacks = 0.1
-			min_time_between_jumps = 0.6
+			min_time_between_jumps = 0.8
 			min_time_chosen_enemy = 3.0
 
 func player_special() -> void:
@@ -86,6 +89,7 @@ func random_t() -> float:
 	return randf_range(0.1, 0.5)
 
 func player_press_direction(dir : Directions) -> void:
+	is_moving = true
 	match dir:
 		Directions.Left:
 			player.left_pressed = true
@@ -129,7 +133,7 @@ func attack_enemy(use_chosen_enemy := true) -> void:
 	if reacting_time < reaction_time:
 		return
 	reacting_time = 0.0
-	print("ai attack_enemy -> chosen enemy:", chosen_enemy)
+	#print("ai attack_enemy -> chosen enemy:", chosen_enemy)
 	var enemy_pos_diff : Vector2
 	if use_chosen_enemy:
 		if not check_chosen_enemy():
@@ -143,36 +147,48 @@ func attack_enemy(use_chosen_enemy := true) -> void:
 		if enemy_pos_diff.x < 0.0: # enemy to the right
 			if enemy_pos_diff.x > -max_attack_radius:
 				if player.facing_right:
-					print("\tattacking enemy to the right: "+ str(enemy_pos_diff.x) + " > -" + str(max_attack_radius))
+					#print("\tattacking enemy to the right: "+ str(enemy_pos_diff.x) + " > -" + str(max_attack_radius))
 					player_attack()
 					return
 				else:
 					player_tap_direction(Directions.Right)
 			else:
-				print("\tenemy to the right, but too far"+str(enemy_pos_diff.x) + " <= -" + str(max_attack_radius))
+				pass
+				#print("\tenemy to the right, but too far: "+str(enemy_pos_diff.x) + " <= -" + str(max_attack_radius))
 		elif enemy_pos_diff.x < max_attack_radius:
 			if not player.facing_right:
-				print("\tattacking enemy to the left: "+ str(enemy_pos_diff.x) + " < " + str(max_attack_radius))
+				#print("\tattacking enemy to the left: "+ str(enemy_pos_diff.x) + " < " + str(max_attack_radius))
 				player_attack()
 				return
 			else:
 				player_tap_direction(Directions.Left)
 		else:
-			print("\tenemy to the left, but too far"+ str(enemy_pos_diff.x) + " >= " + str(max_attack_radius))
+			pass
+			#print("\tenemy to the left, but too far: "+ str(enemy_pos_diff.x) + " >= " + str(max_attack_radius))
 	
 	# movement
 	if enemy_pos_diff.x > min_attack_radius:
-		print("\tgoing left"+str(enemy_pos_diff.x) + " > " + str(min_attack_radius))
+		#print("\tgoing left"+str(enemy_pos_diff.x) + " > " + str(min_attack_radius))
 		player_press_direction(Directions.Left)
 	elif enemy_pos_diff.x < -min_attack_radius:
-		print("\tgoing right"+str(enemy_pos_diff.x) + " < -" + str(min_attack_radius))
+		#print("\tgoing right"+str(enemy_pos_diff.x) + " < -" + str(min_attack_radius))
 		player_press_direction(Directions.Right)
 	if enemy_pos_diff.y < -50.0:
-		print("\tEnemy beside me"+str(enemy_pos_diff.y) + " < -50.0")
+		#print("\tEnemy beside me"+str(enemy_pos_diff.y) + " < -50.0")
 		player_press_direction(Directions.Down)
 	elif enemy_pos_diff.y > 50.0:
-		print("\tEnemy above me"+str(enemy_pos_diff.y) + " > 50.0")
+		#print("\tEnemy above me"+str(enemy_pos_diff.y) + " > 50.0")
 		player_jump()
+	
+	# jump/drop down if obstacle detected
+	if is_moving:
+		if time_since_blocked > 0.2 and (
+					abs(player.global_position.x) - abs(last_position.x) < 5.0):
+			player_tap_direction(Directions.Down)
+			player_jump()
+		else:
+			time_since_blocked = 0.0
+			last_position = player.global_position
 
 func update_enemies(delta : float, force_update := false) -> void:
 	# update various timers
@@ -181,6 +197,7 @@ func update_enemies(delta : float, force_update := false) -> void:
 	time_since_attack += delta
 	time_since_special += delta
 	time_since_jump += delta
+	time_since_blocked += delta
 	
 	# random jumps
 	if time_since_jump > time_between_jumps:
@@ -209,9 +226,13 @@ func update_enemies(delta : float, force_update := false) -> void:
 
 func _on_timer_left_timeout() -> void:
 	player.left_pressed = false
+	if not player.right_pressed:
+		is_moving = false
 
 func _on_timer_right_timeout() -> void:
 	player.right_pressed = false
+	if not player.left_pressed:
+		is_moving = false
 
 func _on_timer_up_timeout() -> void:
 	player.up_pressed = false
@@ -239,8 +260,9 @@ func _on_timer_chosen_enemy_timeout() -> void:
 			break
 	$TimerChosenEnemy.start(randf_range(min_time_chosen_enemy, min_time_chosen_enemy*3))
 
-func _process(_delta: float) -> void:
-	pass
+
+
+
 	# actions:
 		# move (left/right/drop down)
 		# jump/stop_jump
