0024637ccb49d86ba77bb82aaddc02ce5b1608c9
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-12-07 19:00:00 +0100

    wip evolving mode button

--- scripts/evolutions_button.gd
+++ scripts/evolutions_button.gd
@@ -1,11 +1,18 @@
 extends Button
 # evolution button
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	display_evolve_mode()
 
+func display_evolve_mode() -> void:
+	$Label.text = str(GameInfos.EvolvingMode.values()[GameInfos.evolving_mode])
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_pressed() -> void:
+	match GameInfos.evolving_mode:
+		GameInfos.EvolvingMode.Linear:
+			GameInfos.evolving_mode = GameInfos.EvolvingMode.Random
+		GameInfos.EvolvingMode.Random:
+			GameInfos.evolving_mode = GameInfos.EvolvingMode.Fixed
+		GameInfos.EvolvingMode.Fixed:
+			GameInfos.evolving_mode = GameInfos.EvolvingMode.Linear
+	display_evolve_mode()
