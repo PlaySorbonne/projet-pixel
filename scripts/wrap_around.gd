8ee7d1f1c343b6957043a1cf25a452eee2b99a17
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-12-13 15:34:00 +0100

    debug teleporter targets

--- scripts/wrap_around.gd
+++ scripts/wrap_around.gd
@@ -34,10 +34,10 @@ const MAX_UPDATE_TIME := 0.5
 		left_limit = left_limit
 
 var update_time := 0.0
-@onready var top_tp_target    := Vector2(0, top_limit + teleporter_offset)
-@onready var bottom_tp_target := Vector2(0, bottom_limit - teleporter_offset)
-@onready var left_tp_target   := Vector2(left_limit + teleporter_offset, 0)
-@onready var right_tp_target  := Vector2(right_limit - teleporter_offset, 0)
+@onready var top_tp_target    := top_limit + teleporter_offset
+@onready var bottom_tp_target := bottom_limit - teleporter_offset
+@onready var left_tp_target   := left_limit + teleporter_offset
+@onready var right_tp_target  := right_limit - teleporter_offset
  
 
 func _ready() -> void:
@@ -61,17 +61,16 @@ func _process(delta: float) -> void:
 func check_obj_position(obj : Node2D, force_pos := false) -> void:
 	var obj_pos := obj.global_position
 	if   obj_pos.x > right_limit:
-		set_pos(obj, left_tp_target, force_pos)
+		set_pos(obj, Vector2(left_tp_target, obj_pos.y), force_pos)
 	elif obj_pos.x < left_limit:
-		set_pos(obj, right_tp_target, force_pos)
+		set_pos(obj, Vector2(right_tp_target, obj_pos.y), force_pos)
 	if   obj_pos.y > bottom_limit:
-		set_pos(obj, top_tp_target, force_pos)
+		set_pos(obj, Vector2(obj_pos.x, top_tp_target), force_pos)
 	elif obj_pos.y < top_limit:
-		set_pos(obj, bottom_tp_target, force_pos)
+		set_pos(obj, Vector2(obj_pos.x, bottom_tp_target), force_pos)
 
-func set_pos(obj : Node2D, pos_offset : Vector2, force_pos := false) -> void:
-	var true_pos := obj.global_position + pos_offset
+func set_pos(obj : Node2D, new_pos : Vector2, force_pos := false) -> void:
 	if force_pos:
-		obj.force_position(true_pos)
+		obj.force_position(new_pos)
 	else:
-		obj.global_position = true_pos
+		obj.global_position = new_pos
