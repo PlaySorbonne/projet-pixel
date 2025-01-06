164a162d9ef543054c5203c67d9b44a38c20a7a7
Pablo <contact@pablo.town>
2025-01-06 15:09:29 +0100

    change icons in tutorial

--- scripts/tutorial.gd
+++ scripts/tutorial.gd
@@ -6,20 +6,34 @@ const XBOX_STICK_L = "[img]res://resources/images/interface/Xbox Series/Default/
 const XBOX_A = "[img]res://resources/images/interface/Xbox Series/Default/xbox_button_a_outline.png[/img]"
 const XBOX_B = "[img]res://resources/images/interface/Xbox Series/Default/xbox_button_b_outline.png[/img]"
 const XBOX_X = "[img]res://resources/images/interface/Xbox Series/Default/xbox_button_x_outline.png[/img]"
+const KEY_SHIFT = "[img]res://resources/images/interface/Keyboard & Mouse/Default/keyboard_shift_icon.png[/img]"
+const KEY_RETURN = "[img]res://resources/images/interface/Keyboard & Mouse/Default/keyboard_return.png[/img]"
+const KEY_SPACE = "[img]res://resources/images/interface/Keyboard & Mouse/Default/keyboard_space_icon.png[/img]"
+const KEY_ARROWS = "[img]res://resources/images/interface/Keyboard & Mouse/Default/keyboard_arrows.png[/img]"
 
 # TODO: localize text
 var STEPS_TEXTS = [
-	"Bienvenue dans Decorporate ! Vous pouvez vous déplacer avec %s et sauter avec %s." % [XBOX_STICK_L, XBOX_A],
-	"Vous pouvez utiliser votre attaque normale avec %s, et votre attaque spéciale avec %s." % [XBOX_X, XBOX_B],
+	"Bienvenue dans Decorporate ! Vous pouvez vous déplacer avec %s et sauter avec %s.",
+	"Vous pouvez utiliser votre attaque normale avec %s, et votre attaque spéciale avec %s.",
 	"Lorsque vous éliminez un adversaire, vous désévoluez. Atteignez l'évolution finale : le weeb pour continuer.",
 	"Lorsque vous êtes weeb, vous pouvez attaquer la cassette d'anime pour tenter de vous en emparer.",
 	"Une fois la cassette obtenue, vous fusionnez avec elle pour devenir un weeb exalté. Vous pouvez éliminer vos adversaires définitivement."
 ]
 
+const CONTROL_ICONS = [
+	[[KEY_ARROWS, KEY_SPACE], [XBOX_STICK_L, XBOX_A]],
+	[[KEY_RETURN, KEY_SHIFT], [XBOX_X, XBOX_B]],
+]
+
+enum ControlTypes {
+	KEYBOARD,
+	CONTROLLER
+}
+
 var current_step: int = 0
 var player_id: int = 0
 var ennemy_id: int = 0
-
+var control_type = ControlTypes.KEYBOARD
 
 var actions_done = {
 	"jump": false,
@@ -30,6 +44,9 @@ var actions_done = {
 	"ultimate_weeb": false,
 	"win": false
 }
+
+func get_step_text(n):
+	return STEPS_TEXTS[n] % CONTROL_ICONS[n][control_type] if n <= 1 else STEPS_TEXTS[n]
 	
 func init_step_2():
 	GameInfos.players[ennemy_id].spawn($EnnemySpawnLocation.position)
@@ -43,12 +60,14 @@ func init_step_4():
 
 func show_step_text(step: int):
 	var tutorial_text := $TutorialText
-	tutorial_text.text = STEPS_TEXTS[step]
+	tutorial_text.text = get_step_text(step)
 	tutorial_text.visible_ratio = 0.0
 	var tween := create_tween()
 	tween.tween_property(tutorial_text, "visible_ratio", 1.1, 2.0) # 1.1 instead of 1.0 to get the last character (idk why)
 
 func _ready() -> void:
+	if len(Input.get_connected_joypads()) >= 1:
+		control_type = ControlTypes.CONTROLLER
 	GameInfos.anime_box.toggle_cassette(false)
 	for p: PlayerCharacter in GameInfos.players.values():
 		if p.current_evolution == PlayerCharacter.Evolutions.CEO:
