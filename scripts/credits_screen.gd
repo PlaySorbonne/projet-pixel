f6490287347fa61ad15283323c3dc9e256041e62
Maxou <mm.maxencemaire@gmail.com>
2025-01-09 15:52:47 +0100

    wip credits & achievements screens

--- scripts/credits_screen.gd
+++ scripts/credits_screen.gd
@@ -7,3 +7,24 @@ signal ButtonBackPressed
 func _on_button_back_pressed():
 	$ButtonBack.release_focus()
 	emit_signal("ButtonBackPressed")
+
+
+"""
+Tout le monde:
+	
+"""
+
+
+
+"""
+
+Doublage: 
+	 Annonceur: Octave Donatella
+	 CEO: Firion
+	 Manager: Nathaaaan
+	 Employée: Athenaïs
+	 Mascotte: Maxence Maire
+	 Weeb: Loona Macabre
+
+M
+"""
